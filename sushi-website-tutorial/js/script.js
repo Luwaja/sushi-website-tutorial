89f4c48894d28142d16127a2819f32fc2109751b
Luke James <luke.james924@gmail.com>
2024-02-05 18:23:12 -0600

    header initialized

--- sushi-website-tutorial/js/script.js
+++ sushi-website-tutorial/js/script.js
@@ -1,8 +1,8 @@
-// import AOS from "aos";
-// import "aos/dist/aos.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-// // init AOS animation
-// AOS.init({
-//     duration: 1000,
-//     offset: 100,
-// });
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
